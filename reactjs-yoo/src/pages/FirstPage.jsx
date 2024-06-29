8d176ab6ab5e265c3989587404c63c321b72d2ef
AbhinavGurrapu <abhinavgurrapu09@gmail.com>
2024-06-29 11:32:20 +0530

    somechanges

--- reactjs-yoo/src/pages/FirstPage.jsx
+++ reactjs-yoo/src/pages/FirstPage.jsx
@@ -5,20 +5,17 @@ export default function landingpage() {
   return (
     <html lang="en">
       <head>
-        <meta charSet="UTF-8" />
-        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
         <title>landing page</title>
-        {/* Include Tailwind CSS */}
         <script src="https://cdn.tailwindcss.com"></script>
       </head>
       <body>
         <div className="bg-gradient-to-r from-indigo-500 via-sky-500 to-emerald-500 h-14 flex justify-between">
           <p className="text-4xl font-serif pt-2 px-2 font-semibold">CodeBlitz</p>
-          <ul className="flex justify-end">
-            <li className="cursor-pointer px-5 py-4 hover:bg-gradient-to-r from-pink-500 to-yellow-500 rounded-xl text-xl font-semibold">Compete</li>
-            <li className="cursor-pointer px-5 py-4 hover:bg-gradient-to-r from-pink-500 to-yellow-500 rounded-xl text-xl font-semibold">Practice</li>
-            <li className="cursor-pointer px-5 py-4 hover:bg-gradient-to-r from-pink-500 to-yellow-500 rounded-xl text-xl font-semibold">Contests</li>
-         <Link to="/SignIn">   <li className="cursor-pointer px-5 py-4 hover:bg-gradient-to-r from-pink-500 to-yellow-500 rounded-xl text-xl font-semibold">Login</li></Link>
+          <ul className="flex justify-end ">
+            <li className="cursor-pointer px-5 py-3 hover:bg-gradient-to-r from-pink-500 to-yellow-500 rounded-xl text-xl font-semibold">Compete</li>
+            <li className="cursor-pointer px-5 py-3 hover:bg-gradient-to-r from-pink-500 to-yellow-500 rounded-xl text-xl font-semibold">Practice</li>
+            <li className="cursor-pointer px-5 py-3 hover:bg-gradient-to-r from-pink-500 to-yellow-500 rounded-xl text-xl font-semibold">Contests</li>
+         <Link to="/SignIn">   <li className="cursor-pointer px-5 py-3 hover:bg-gradient-to-r from-pink-500 to-yellow-500 rounded-xl text-xl font-semibold">Login</li></Link>
           </ul>
         </div>
         <main className="bg-sky-500 flex">
@@ -28,11 +25,11 @@ export default function landingpage() {
               Gives info about the contests on various sites such as Codeforces, AtCoder, LeetCode, CodeChef, and GeeksForGeeks
             </p>
             <div className="pl-40 m-10 font-medium">
-            <Link to="SignUp">  <button className="bg-indigo-500 hover:bg-gradient-to-r from-pink-500 to-yellow-500 rounded-xl">Create Account {'>'}</button></Link>
+            <Link to="SignUp">  <button className="bg-indigo-500 hover:bg-gradient-to-r from-pink-500 to-yellow-500 rounded-xl py-2 px-4">Create Account {'>'}</button></Link>
             </div>
           </div>
           <div className="flex items-center">
-            <img src="Untitled design.png" alt="Design" className="h-fit" />
+            <img src="Untitled design.png" alt="Design" className="h-fit " />
           </div>
 
         </main>
