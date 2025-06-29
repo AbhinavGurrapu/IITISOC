2ea5c08862a23bedae4dbe1c6174ec7168dbfa32
mokshith27 <mokshithmsd@gmail.com>
2025-06-29 12:37:50 +0530

    database

--- reactjs-yoo/backend/db.js
+++ reactjs-yoo/backend/db.js
@@ -1,6 +1,7 @@
 const mongoose = require('mongoose');
+require('dotenv').config({ path: __dirname + '/.env' });
 
-const MONGO_URI = 'mongodb://localhost:27017/codeblitz';
+const MONGO_URI = process.env.MONGODB_URI;
 
 function connectDB() {
   mongoose.connect(MONGO_URI, {
